9bf19c90ec800251585b7a15b8688e217ba5252c
Carmen de Lope <cdelope@daisho.group>
2019-08-07 10:28:33 +0200

    NP-1793 getEICClient returns the ClientConn to close it

--- internal/app/eicproxy/server/ecproxy/manager.go
+++ internal/app/eicproxy/server/ecproxy/manager.go
@@ -56,26 +56,29 @@ func (m*Manager) getIP(dnsName string) (*string, error){
 	return nil, errors.New("empty result")
 }
 
-func (m*Manager) getEICClient(edgeControllerID string) (grpc_edge_controller_go.EICClient, derrors.Error){
+func (m*Manager) getEICClient(edgeControllerID string) (grpc_edge_controller_go.EICClient, *grpc.ClientConn, derrors.Error){
 	dnsName := fmt.Sprintf("%s-vpn.service.nalej", edgeControllerID)
 	ecIP, err := m.getIP(dnsName)
 	if err != nil{
-		return nil, derrors.AsError(err, "cannot resolve IP address for EC")
+		return nil, nil, derrors.AsError(err, "cannot resolve IP address for EC")
 	}
 	ipAddr := fmt.Sprintf("%s:5577", *ecIP)
 	log.Debug().Str("DNS", dnsName).Str("IP", ipAddr).Msg("Creating agent client")
 	conn, err := grpc.Dial(ipAddr, grpc.WithInsecure())
 	if err != nil {
-		return nil, derrors.AsError(err, "cannot create connection with edge controller")
+		return nil, nil, derrors.AsError(err, "cannot create connection with edge controller")
 	}
 	client := grpc_edge_controller_go.NewEICClient(conn)
-	return client, nil
+	return client, conn, nil
 }
 
 func (m*Manager) InstallAgent(request *grpc_inventory_manager_go.InstallAgentRequest) (*grpc_inventory_manager_go.EdgeControllerOpResponse, error) {
-	edgeClient, aErr := m.getEICClient(request.EdgeControllerId)
+	edgeClient, conn, aErr := m.getEICClient(request.EdgeControllerId)
 	if aErr != nil{
 		return nil, conversions.ToGRPCError(aErr)
+	}
+	if conn != nil {
+		defer conn.Close()
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), ControllerTimeout)
 	defer cancel()
@@ -88,9 +91,12 @@ func (m*Manager) InstallAgent(request *grpc_inventory_manager_go.InstallAgentReq
 }
 
 func (m*Manager) CreateAgentJoinToken(edgeControllerID *grpc_inventory_go.EdgeControllerId) (*grpc_inventory_manager_go.AgentJoinToken, error) {
-	edgeClient, aErr := m.getEICClient(edgeControllerID.EdgeControllerId)
+	edgeClient, conn, aErr := m.getEICClient(edgeControllerID.EdgeControllerId)
 	if aErr != nil{
 		return nil, conversions.ToGRPCError(aErr)
+	}
+	if conn != nil {
+		defer conn.Close()
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), ControllerTimeout)
 	defer cancel()
@@ -103,9 +109,12 @@ func (m*Manager) CreateAgentJoinToken(edgeControllerID *grpc_inventory_go.EdgeCo
 }
 
 func (m*Manager) TriggerAgentOperation(request *grpc_inventory_manager_go.AgentOpRequest) (*grpc_inventory_manager_go.AgentOpResponse, error) {
-	edgeClient, aErr := m.getEICClient(request.EdgeControllerId)
+	edgeClient, conn, aErr := m.getEICClient(request.EdgeControllerId)
 	if aErr != nil{
 		return nil, conversions.ToGRPCError(aErr)
+	}
+	if conn != nil {
+		defer conn.Close()
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), ControllerTimeout)
 	defer cancel()
@@ -121,9 +130,12 @@ func (m*Manager) Configure(request *grpc_inventory_manager_go.ConfigureEICReques
 }
 
 func (m*Manager) ListMetrics(selector *grpc_inventory_go.AssetSelector) (*grpc_monitoring_go.MetricsList, error) {
-	edgeClient, aErr := m.getEICClient(selector.GetEdgeControllerId())
+	edgeClient, conn, aErr := m.getEICClient(selector.GetEdgeControllerId())
 	if aErr != nil{
 		return nil, conversions.ToGRPCError(aErr)
+	}
+	if conn != nil {
+		defer conn.Close()
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), ControllerTimeout)
 	defer cancel()
@@ -135,9 +147,12 @@ func (m*Manager) ListMetrics(selector *grpc_inventory_go.AssetSelector) (*grpc_m
 }
 
 func (m*Manager) QueryMetrics(request *grpc_monitoring_go.QueryMetricsRequest) (*grpc_monitoring_go.QueryMetricsResult, error) {
-	edgeClient, aErr := m.getEICClient(request.GetAssets().GetEdgeControllerId())
+	edgeClient, conn, aErr := m.getEICClient(request.GetAssets().GetEdgeControllerId())
 	if aErr != nil{
 		return nil, conversions.ToGRPCError(aErr)
+	}
+	if conn != nil {
+		defer conn.Close()
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), ControllerTimeout)
 	defer cancel()
@@ -151,9 +166,12 @@ func (m*Manager) QueryMetrics(request *grpc_monitoring_go.QueryMetricsRequest) (
 func (m *Manager) UnlinkEC(edge *grpc_inventory_go.EdgeControllerId) (*grpc_common_go.Success, error){
 	log.Debug().Msg("UnlinkEIC received")
 
-	edgeClient, aErr := m.getEICClient(edge.EdgeControllerId)
+	edgeClient, conn, aErr := m.getEICClient(edge.EdgeControllerId)
 	if aErr != nil{
 		return nil, conversions.ToGRPCError(aErr)
+	}
+	if conn != nil {
+		defer conn.Close()
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), ControllerTimeout)
 	defer cancel()
@@ -166,11 +184,13 @@ func (m *Manager) UnlinkEC(edge *grpc_inventory_go.EdgeControllerId) (*grpc_comm
 }
 
 func (m *Manager) UninstallAgent(assetID *grpc_inventory_manager_go.FullUninstallAgentRequest) (*grpc_inventory_manager_go.EdgeControllerOpResponse, error) {
-	edgeClient, aErr := m.getEICClient(assetID.EdgeControllerId)
+	edgeClient, conn, aErr := m.getEICClient(assetID.EdgeControllerId)
 	if aErr != nil{
 		return nil, conversions.ToGRPCError(aErr)
 	}
-
+	if conn != nil {
+		defer conn.Close()
+	}
 	ctx, cancel := context.WithTimeout(context.Background(), ControllerTimeout)
 	defer cancel()
 
